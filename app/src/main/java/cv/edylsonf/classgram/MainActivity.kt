9433b285f92b80357a6a95866d313f6b0c539fcd
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-02-13 23:16:53 +0000

    import not working

--- app/src/main/java/cv/edylsonf/classgram/MainActivity.kt
+++ app/src/main/java/cv/edylsonf/classgram/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*bottomNavigationView.background = null*/
+        /*bottomNavigationView.menu.getItem(2).isEnabled = false*/
+
     }
 }
