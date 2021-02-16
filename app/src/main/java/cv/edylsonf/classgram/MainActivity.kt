97765a631d5f3a9abb521049b276148793580e69
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-02-16 07:16:53 +0000

    add view model

--- app/src/main/java/cv/edylsonf/classgram/MainActivity.kt
+++ app/src/main/java/cv/edylsonf/classgram/MainActivity.kt
@@ -2,14 +2,41 @@ package cv.edylsonf.classgram
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import cv.edylsonf.classgram.presentation.views.TweetFragment
 
 class MainActivity : AppCompatActivity(){
+
+    private val tweetFragment by lazy {TweetFragment()}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         /*bottomNavigationView.background = null*/
         /*bottomNavigationView.menu.getItem(2).isEnabled = false*/
 
+        //loadFragment(TweetFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.home_button -> {
+                    loadFragment(tweetFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
+
+    private fun loadFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navigationContainer, fragment)
+            .commit()
+    }
+
 }
--- app/src/main/java/cv/edylsonf/classgram/network/models/Tweet.kt
+++ app/src/main/java/cv/edylsonf/classgram/network/models/Tweet.kt
@@ -2,9 +2,13 @@ package cv.edylsonf.classgram.network.models
 
 import com.squareup.moshi.Json
 
+
 data class Tweet(
     @field:Json(name = "data")
     val `data`: List<Data>,
     @field:Json(name = "includes")
-    val includes: Includes
+    val includes: Includes,
+
+    @Transient
+    val id: Int,
 )
