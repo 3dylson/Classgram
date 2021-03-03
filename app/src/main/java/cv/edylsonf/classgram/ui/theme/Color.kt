7b7261eeff6e3025c5472a28788341d326d1f720
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-03-03 14:03:43 +0000

    UI Theme with Composable

---
+++ app/src/main/java/cv/edylsonf/classgram/ui/theme/Color.kt
@@ -0,0 +1,44 @@
+package cv.edylsonf.classgram.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.ui.graphics.Color
+
+val primaryColor = Color(0x00bbd3)
+val primaryLightColor = Color(0x62eeff)
+val primaryDarkColor = Color(0x008ba2)
+val secondaryColor = Color(0xafbdc4)
+val secondaryLightColor = Color(0xe1eff7)
+val secondaryDarkColor = Color(0x808d94)
+val primaryTextColor = Color(0x000000)
+val secondaryTextColor = Color(0x000000)
+
+val darkThemeBlack = Color (0x121212)
+val primaryColor200 = Color(0x80DEEA)
+val darkThemeError = Color(0xCF6679)
+
+
+
+
+/*
+val classgramColorTheme= Colors(
+    primary = primaryColor200,
+    primaryVariant =  primaryDarkColor,
+
+    secondary = secondaryColor,
+    secondaryVariant = secondaryColor,
+
+    background = darkThemeBlack,
+    surface = darkThemeBlack,
+
+    error = darkThemeError,
+
+    onPrimary = primaryTextColor,
+    onSecondary = secondaryTextColor,
+
+    onBackground = white,
+    onSurface = white,
+    onError = black,
+
+    isLight = false
+
+)*/
