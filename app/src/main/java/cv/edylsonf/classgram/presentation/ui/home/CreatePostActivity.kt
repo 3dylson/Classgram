5bed6348bd55b58549381b10d35566d19adeb43d
Edylson Frederico <Edylson_Frederico@outlook.com>
2021-08-14 07:45:09 +0200

    add menu action and init gallery open imp

--- app/src/main/java/cv/edylsonf/classgram/presentation/ui/home/CreatePostActivity.kt
+++ app/src/main/java/cv/edylsonf/classgram/presentation/ui/home/CreatePostActivity.kt
@@ -1,8 +1,14 @@
 package cv.edylsonf.classgram.presentation.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.core.view.setPadding
-import androidx.core.view.updatePadding
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
+import cv.edylsonf.classgram.PICK_PHOTO_CODE
 import cv.edylsonf.classgram.R
 import cv.edylsonf.classgram.databinding.ActivityCreatePostBinding
 import cv.edylsonf.classgram.domain.models.User
@@ -12,19 +18,21 @@ private const val TAG = "CreatePostActivity"
 
 class CreatePostActivity : BaseActivity() {
 
-    private var signedInUser: User? = null
-
     private lateinit var binding: ActivityCreatePostBinding
+
+    private var signedInUser: User? = null
+    private var photoUri: Uri? = null
+    private lateinit var database: FirebaseFirestore
+    private lateinit var storageReference: StorageReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_Classgram)
         binding = ActivityCreatePostBinding.inflate(layoutInflater)
-        binding.root.setPadding(16)
         setContentView(binding.root)
 
-        supportActionBar?.title = "Post"
+        supportActionBar?.title = "Create Post"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_close)
 
@@ -34,8 +42,25 @@ class CreatePostActivity : BaseActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.create_post_action_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.submitPost) {
+            // ...
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun openGallery() {
-        TODO("Not yet implemented")
+        Log.i(TAG, "Open up image picker on device")
+        val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
+        imagePickerIntent.type = "image/*"
+        if (imagePickerIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(imagePickerIntent, PICK_PHOTO_CODE)
+        }
     }
 
     private fun openCam() {
