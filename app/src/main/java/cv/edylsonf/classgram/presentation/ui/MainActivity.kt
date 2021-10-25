40990e3f560c162152b4e9f0cfd0380d30cf8ec5
Edylson Frederico <E5957@itsector.pt>
2021-10-25 16:33:28 +0100

    feat(splashScreen): handle splash screen
    when user launches the app it check whether user is signed in or not and
    updates UI

--- app/src/main/java/cv/edylsonf/classgram/presentation/ui/MainActivity.kt
+++ app/src/main/java/cv/edylsonf/classgram/presentation/ui/MainActivity.kt
@@ -40,7 +40,6 @@ class MainActivity : BaseActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_Classgram)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
---
+++ app/src/main/java/cv/edylsonf/classgram/presentation/ui/login/SplashScreenActivity.kt
@@ -0,0 +1,37 @@
+package cv.edylsonf.classgram.presentation.ui.login
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import cv.edylsonf.classgram.R
+import cv.edylsonf.classgram.presentation.ui.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.splashScreenTheme)
+        auth = Firebase.auth
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val mainIntent = Intent(this, MainActivity::class.java)
+            mainIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(mainIntent)
+            finish()
+        }
+        else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(loginIntent)
+            finish()
+        }
+    }
+}
